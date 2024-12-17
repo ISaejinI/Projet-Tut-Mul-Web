08d2ad54f78d865454584a89ceef2c360dd2e3ae
Lou-Anne <saejinmx@gmail.com>
2024-12-17 13:48:09 +0100

    Countdown et mise en page ok

--- js/main.js
+++ js/main.js
@@ -14,25 +14,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   });
 
-  const countdownTl = gsap.timeline();
+  const launchButton = document.getElementById('launchButton');
+  launchButton.addEventListener("click", (evt) => {
+    gsap.to("#launchButton", {opacity: 0, ease: "sine"});
+    const countdownTl = gsap.timeline();
 
-  countdownTl.to("#nb3", { delay: 1, display: "none" })
-    .to("#nb2", { display: "block" })
-    .to("#nb2", { delay: 1, display: "none" })
-    .to("#nb1", { display: "block" })
-    .to("#nb1", { delay: 1, display: "none" })
-    .to("#nb0", { display: "block" })
+    countdownTl.to("#nb3", { delay: 1, display: "none" })
+      .to("#nb2", { display: "block" })
+      .to("#nb2", { delay: 1, display: "none" })
+      .to("#nb1", { display: "block" })
+      .to("#nb1", { delay: 1, display: "none" })
+      .to("#nb0", { display: "block" })
+  })
 
 
-  gsap.to("#fusee", {
-    y: -50,
-    x: 300,
-    scrollTrigger: {
-      trigger: '#soleil',
-      start: 'top center', // Déclenche l'animation lorsque le haut de #soleil arrive au centre de l'écran
-      end: 'bottom center',
-      scrub: 1, // Animation fluide liée au scroll
-      markers: true, // Garde pour tester, supprime en production
-    }
-  });
+  // gsap.to("#fusee", {
+  //   y: -50,
+  //   x: 300,
+  //   scrollTrigger: {
+  //     trigger: '#soleil',
+  //     start: 'top center', // Déclenche l'animation lorsque le haut de #soleil arrive au centre de l'écran
+  //     end: 'bottom center',
+  //     scrub: 1, // Animation fluide liée au scroll
+  //     markers: true, // Garde pour tester, supprime en production
+  //   }
+  // });
 });
