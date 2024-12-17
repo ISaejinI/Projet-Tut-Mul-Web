c3031b34494698a333b3cacf63c0aa42dc676049
Saejin <saejinmx@gmail.com>
2024-12-17 01:07:34 +0100

    Countdown fonctionnel

--- js/main.js
+++ js/main.js
@@ -10,32 +10,44 @@ document.addEventListener("DOMContentLoaded", (event) => {
       pin: true,
       scrub: 1,
       start: "top top",
-      end: "bottom top",
-      markers: true
+      end: "bottom top"
     }
   });
 
   const countdownTl = gsap.timeline();
 
   countdownTl.to("#nb3", {
+    delay:1,
     display: "none",
   })
   countdownTl.to("#nb2", {
-    delay: 1,
     display: "block",
   })
   countdownTl.to("#nb2", {
+    delay:1,
     display: "none",
   })
   countdownTl.to("#nb1", {
-    delay: 1,
     display: "block",
   })
   countdownTl.to("#nb1", {
+    delay:1,
     display: "none",
   })
   countdownTl.to("#nb0", {
-    delay: 1,
     display: "block",
   })
+  
+
+  gsap.to("#fusee", {
+    y: -50,
+    x: 300,
+    scrollTrigger: {
+      trigger: '#soleil',
+      start: 'top center', // Déclenche l'animation lorsque le haut de #soleil arrive au centre de l'écran
+      end: 'bottom center',
+      scrub: 1, // Animation fluide liée au scroll
+      markers: true, // Garde pour tester, supprime en production
+    }
+  });
 });
