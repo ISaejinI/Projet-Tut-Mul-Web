7c39a66741e5ace9d7913484c0e29974c14534d4
Snay77 <77ethan.barlet@gmail.com>
2024-12-17 13:43:00 +0100

    Ajout de plein de truc

--- js/main.js
+++ js/main.js
@@ -10,10 +10,46 @@ document.addEventListener("DOMContentLoaded", () => {
       trigger: ".wrapper",
       pin: true,
       scrub: 1,
-      snap: 1 / (sections.length - 1),
+      // snap: 1 / (sections.length - 1),
       end: () => "+=" + document.querySelector(".wrapper").offsetWidth
     }
   });
 
-  gsap.to()
+// Fusée touche ciel
+  const rocketTl = gsap.timeline({scrollTrigger: {
+    trigger: '.sol',
+    markers: true,
+    start: () => {
+      const screenHeightTimesThree = window.innerWidth * 1;
+      return `${screenHeightTimesThree}px top`;
+    },
+    end: ()=>{
+      const screenHeightTimesThree = window.innerWidth * 2;
+      return `${screenHeightTimesThree}px top`;
+    },
+    toggleAction: "restart reverse play reverse",
+    scrub: true,
+
+  }});
+  rocketTl.fromTo('#fusee',
+    {
+      x: 0,
+      transform: 'translateY(-50%) rotate(90deg)'
+    }, 
+    {
+      x: 200,
+      y: 300
+  });
+
+// Nuage quand on arrive au ciel
+  const nuageTl = gsap.timeline({scrollTrigger: {
+    trigger: '.ciel',
+    markers: true,
+    start: () => {
+      const screenHeightTimesThree = window.innerWidth * 1;
+      return `${screenHeightTimesThree}px top`;
+    }
+  }});
+
+  nuageTl.fromTo('.nuage', {y:0}, {y:200})
 });
